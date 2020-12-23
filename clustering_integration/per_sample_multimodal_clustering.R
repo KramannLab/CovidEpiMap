022d115b3e333d16f567c40d4f09cc08963cc72f
monicahannani <monicahannani@gmail.com>
2020-12-23 13:59:35 +0100

    minor changes to paths in scripts

--- clustering_integration/per_sample_multimodal_clustering.R
+++ clustering_integration/per_sample_multimodal_clustering.R
@@ -4,15 +4,18 @@
 
 #---- Per-sample clustering and integration of GEX and ADT libraries
 
-#remotes::install_github("satijalab/seurat", ref = "release/4.0.0")
+# Source 
 # https://satijalab.org/seurat/v4.0/weighted_nearest_neighbor_analysis.html
+
+#remotes::install_github("satijalab/seurat", ref = "release/4.0.0")
 library(Seurat)
 library(SeuratData)
 library(genesorteR)
 library(stringr)
 library(ggplot2)
 library(dplyr)
-source('../sc_source/sc_source.R')
+source('sc_source/sc_source.R')
+setwd('~/sciebo/CovidEpiMap/')
 patients = c('1', '2', '3', '5', '6', '7', '11', '12', 
 	'13', '15', '18', '19', '29', '31', '32')
 
@@ -29,7 +32,7 @@ for (patient in patients){
 
 
 	# Read both Gene Expression and Antibody Capture (ADT) assays
-	sc.data = Read10X(data.dir = paste0(sample, '/filtered_feature_bc_matrix'))
+	sc.data = Read10X(data.dir = paste0('data/', sample, '/filtered_feature_bc_matrix'))
 	sc = CreateSeuratObject(counts = sc.data$'Gene Expression', project = sample)
 	sc[['ADT']] = CreateAssayObject(counts = sc.data$'Antibody Capture')
 
@@ -43,7 +46,7 @@ for (patient in patients){
 	features = c('nFeature_RNA', 'nCount_RNA', 'percent.mt', 
 			'percent.ribo', 'nFeature_ADT', 'nCount_ADT')
 
-	pdf(file = paste0(sample, '_QC.pdf'), height = 10, width = 14)
+	pdf(file = paste0(outdir, sample, '_QC.pdf'), height = 10, width = 14)
 	print(VlnPlot(sc, ncol = 3, pt.size = 0,
 		features = features))
 	dev.off()
@@ -92,7 +95,7 @@ for (patient in patients){
 		reduction.name = 'adt.umap', reduction.key = 'adtUMAP_')
 
 
-	pdf(file = paste0(sample, '_clustering.pdf'))
+	pdf(file = paste0(outdir, sample, '_clustering.pdf'))
 	print(DimPlot(sc, reduction = 'wnn.umap', label = TRUE))
 	print(DimPlot(sc, reduction = 'rna.umap', label = TRUE, repel = TRUE) + NoLegend())
 	print(DimPlot(sc, reduction = 'adt.umap', label = TRUE, repel = TRUE) + NoLegend())
@@ -101,7 +104,7 @@ for (patient in patients){
 
 
 	# QC plots after filtering and clustering
-	pdf(file = paste0(sample, '_QC_afterfiltering.pdf'), height = 10, width = 14)
+	pdf(file = paste0(outdir, sample, '_QC_afterfiltering.pdf'), height = 10, width = 14)
 	print(VlnPlot(sc, ncol = 3, pt.size = 0,
 		features = features))
 	print(FeaturePlot(sc, reduction = 'wnn.umap', ncol = 3, 
@@ -113,7 +116,7 @@ for (patient in patients){
 	# Visualize counts from ADT library (dextramer + CITE seq)
 	DefaultAssay(sc) = 'ADT'
 	markers = rownames(sc[['ADT']])
-	pdf(file = paste0(sample, '_ADT_Markers.pdf'), width = 8)
+	pdf(file = paste0(outdir, sample, '_ADT_Markers.pdf'), width = 8)
 	print(DoHeatmap(sc, features = markers, assay = 'ADT', raster = FALSE, size = 3))
 	for (marker in markers){
 		print(FeaturePlot(sc, feature = marker, reduction = 'wnn.umap'))
@@ -140,7 +143,7 @@ for (patient in patients){
 
 	height = length(top.genes) * 0.2
 	width = length(levels(Idents(sc))) * 0.5
-	pdf(file = paste0(sample, '_GeneSorterTop10.pdf'),
+	pdf(file = paste0(outdir, sample, '_GeneSorterTop10.pdf'),
 		height = height, width = width)
 	print(DotPlot(sc, features = rev(top.genes), assay = 'RNA') + coord_flip())
 	dev.off()
@@ -171,17 +174,18 @@ for (patient in patients){
 
 	# Save data
 	DefaultAssay(sc) = 'RNA'
-	saveRDS(sc, file = paste0(sample, '.rds'))
+	saveRDS(sc, file = paste0(outdir, sample, '.rds'))
 
 }
 
 
 
 #---- Add TCR clonotypes to Seurat objects
-# From https://www.biostars.org/p/383217/
+
+# Source
+# https://www.biostars.org/p/383217/
 
 # Run addVDJ.py script prior to the following
-
 
 for (patient in patients){
 
@@ -197,7 +201,7 @@ for (patient in patients){
 	
 	# Read VDJ info
 	sample_vdj = paste0(patient, '_', vdj_type)
-	vdj = read.csv(file = paste0(sample_vdj, '/filtered_contig_annotations.csv'))
+	vdj = read.csv(file = paste0('data/', sample_vdj, '/filtered_contig_annotations.csv'))
 
 	# Subset so only first line of each barcode is kept (they have the same clonotype id anyway)
 	vdj = vdj[!duplicated(vdj$barcode),]
@@ -205,7 +209,7 @@ for (patient in patients){
 	names(vdj)[names(vdj) == 'raw_clonotype_id'] = 'clonotype_id'
 
 	# Get clonotype info
-	clono = read.csv(file = paste0(sample_vdj, '/clonotypes.csv'))
+	clono = read.csv(file = paste0('data/', sample_vdj, '/clonotypes.csv'))
 
 	# Add amino acid sequence of CDR3s to data frame
 	vdj = merge(vdj, clono[,c('clonotype_id', 'cdr3s_aa')])
@@ -229,7 +233,7 @@ for (patient in patients){
 
 	# Add clonotype information to Seurat object
 	sc = AddMetaData(object = sc, metadata = vdj)
-	saveRDS(sc, file = paste0(sample, '.rds'))
+	saveRDS(sc, file = paste0(indir, sample, '.rds'))
 }
 
 
--- epitope_data_analysis/epitope_analysis.R
+++ epitope_data_analysis/epitope_analysis.R
@@ -10,15 +10,28 @@ source('sc_source/sc_source.R')
 
 sc = readRDS(file = paste0(indir, 'integrated.RNA.Tcells.annotated.rds'))
 DefaultAssay(sc) = 'RNA'
-
-
-# Read epitope-binding/clonotype data
+Idents(sc) = 'integrated_annotations'
 conditions = levels(sc$condition)
-tables = lapply(conditions, function(x){
+
+
+# Read epitope-binding data (single-cell-level)
+cell.tables = lapply(conditions, function(x){
 				read.table(file = paste0(datdir, x, '_bycell-selected.csv'), 
 					sep = ',', header = TRUE)})
+cell.table = do.call(rbind, cell.tables)
 
 
-# Subset to only keep cells with enriched dextramer binding compared to negative control
-table = do.call(rbind, tables)
-table = table[which(table$Log2FC > 0),]
+# Read epitope-binding data (clonotype-level)
+# Only clonotypes with size > 5 are included in the tables
+clonotype.tables = lapply(conditions, function(x){
+				read.table(file = paste0(datdir, x, '_byclone-selected.csv'), 
+					sep = ',', header = TRUE)})
+clonotype.table = do.call(rbind, clonotype.tables)
+
+
+
+# Subset to only keep cell with enriched dextramer-binding compared to negative control (L2FC > 2)
+
+table = table[which(table$Log2FC > 2),]
+
+
