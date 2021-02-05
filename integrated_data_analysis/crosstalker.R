bd048910dbb34485d50906bf751ec1c73c135246
monicahannani <monicahannani@gmail.com>
2021-02-05 12:37:51 +0100

    clean up crosstalker script, add selected sankey plots

--- integrated_data_analysis/crosstalker.R
+++ integrated_data_analysis/crosstalker.R
@@ -5,28 +5,12 @@
 #---- Visualize CellPhoneDB results with CrossTalkeR
 
 source('sc_source/sc_source.R')
+source('sc_source/crosstalker_source.R')
 source.folder = '~/sciebo/CovidEpiMap/CrossTalkeR/CrossTalkeR/'
 devtools::load_all(source.folder)
 wkdir = '~/sciebo/CovidEpiMap/CrossTalkeR/'
 indir = 'significant_means/filtered_corrected/'
 setwd(wkdir)
-
-
-crosstalker_report = function(Control, Case, Genes, Indir, Outdir){
-	# Define path for control and case (CellPhoneDB has been run separately on these)
-	# Files have already been filtered and corrected with format.py from CrossTalkeR package
-	paths = c('CTR' = paste0(Indir, Control, '_filtered_corrected.csv'),
-			'EXP' = paste0(Indir, Case, '_filtered_corrected.csv'))
-
-	# Generate report
-	outdir = paste0(Outdir, '/', Control, '_vs_', Case, '/')
-
-	generate_report(lrpaths = paths,
-					out_path = outdir,
-					threshold = 0,
-					out_file = paste0(Control, '_vs_', Case, '.html'),
-					report = TRUE)
-}
 
 
 # Active mild vs active severe
@@ -42,12 +26,11 @@ data_recov = crosstalker_report(Control = control, Case = case, Indir = indir, O
 
 
 
-#---- Save selected plots from CrossTalkeR (active severe vs active mild)
+#---- Single cell-cell interaction plot (active mild and active severe)
 
 outdir = paste0(wkdir, 'active_mild_vs_active_severe/')
 
-
-# Single cell-cell interaction plot (active mild)
+# Active mild
 pdf(file = paste0(outdir, 'single_cci_plot_active_mild.pdf'), width = 10, height = 10)
 # Modified function to fit coloring scheme
 plot_cci(graph = data_active@graphs$CTR,
@@ -64,8 +47,7 @@ plot_cci(graph = data_active@graphs$CTR,
          vfactor = 12)
 dev.off()
 
-
-# Single cell-cell interaction plot (active severe)
+# Active severe
 pdf(file = paste0(outdir, 'single_cci_plot_active_severe.pdf'), width = 10, height = 10)
 plot_cci(graph = data_active@graphs$EXP,
          colors = cell.type.colors,
@@ -82,7 +64,9 @@ plot_cci(graph = data_active@graphs$EXP,
 dev.off()
 
 
-# Differential cell-cell interaction plot
+
+#---- Differential cell-cell interaction plot (active mild vs active severe)
+
 pdf(file = paste0(outdir, 'differential_cci_plot.pdf'), width = 10, height = 10)
 plot_cci(graph = data_active@graphs$EXP_x_CTR,
          colors = cell.type.colors,
@@ -100,54 +84,9 @@ dev.off()
 
 
 
-# Plot differential cell-cell ligand/receptor frequency table
+#---- Differential cell-cell ligand/receptor frequency table (active mild vs active severe)
+
 all_data = readRDS(file = paste0(outdir, 'LR_data_step2.Rds'))
-
-# Plotting script from CrossTalkeR (modified)
-plot_table = function(all_data = all_data){
-    curr <- "EXP_x_CTR"
-    curr_net <- all_data@graphs[[curr]]
-    in_deg_up <- table(all_data@tables[[curr]]$Ligand.Cluster[all_data@tables[[curr]]$MeanLR > 0])
-    in_up <- tibble::tibble(as.data.frame(in_deg_up))
-    in_deg_down <- table(all_data@tables[[curr]]$Ligand.Cluster[all_data@tables[[curr]]$MeanLR < 0])
-    in_down <- tibble::tibble(as.data.frame(in_deg_down))
-    in_down$Freq <- 0-in_down$Freq
-    in_all <- dplyr::bind_rows(in_up,in_down)
-    in_all$Expression <- ifelse(in_all$Freq<0,'Downregulated','Upregulated')
-    in_all$rank <- ifelse(in_all$Freq<0,0,1)
-    out_deg_up <- table(all_data@tables[[curr]]$Receptor.Cluster[all_data@tables[[curr]]$MeanLR > 0])
-    out_up <- tibble::tibble(as.data.frame(out_deg_up))
-    out_deg_down <- table(all_data@tables[[curr]]$Receptor.Cluster[all_data@tables[[curr]]$MeanLR < 0])
-    out_down <- tibble::tibble(as.data.frame(out_deg_down))
-    out_down$Freq <- 0-out_down$Freq
-    out_all <- dplyr::bind_rows(out_up,out_down)
-    out_all$Expression <- ifelse(out_all$Freq<0,'Downregulated','Upregulated')
-    out_all$rank <- ifelse(out_all$Freq<0,0,1)
-    p1 <- ggplot2::ggplot(in_all,ggplot2::aes(x=Freq,y=Var1,fill=Expression))+
-      ggplot2::geom_bar(stat = 'identity',position = "identity", show.legend = FALSE)+
-      ggplot2::geom_text(ggplot2::aes(label=sub("-", "", Freq)),size=3.5)+
-      ggplot2::scale_fill_manual(values=pals::coolwarm(2))+
-      ggplot2::ggtitle('Ligands')+
-      ggplot2::ylab('')+
-      ggplot2::xlab('Number of interactions')+
-      cowplot::theme_cowplot() +
-      theme(axis.line  = element_blank(),
-            axis.ticks = element_blank(),
-            axis.text.x = element_blank())
-    p2 <- ggplot2::ggplot(out_all,ggplot2::aes(x=Freq,y=Var1,fill=Expression))+
-      ggplot2::geom_bar(stat = 'identity',position = "identity")+
-      ggplot2::geom_text(ggplot2::aes(label=sub("-", "", Freq)),size=3.5)+
-      ggplot2::scale_fill_manual(values=pals::coolwarm(2))+
-      ggplot2::ggtitle('Receptors')+
-      ggplot2::ylab('')+
-      ggplot2::xlab('Number of interactions')+
-      cowplot::theme_cowplot() +
-      theme(axis.line  = element_blank(),
-            axis.ticks = element_blank(),
-            axis.text.x = element_blank())
-    return(p1+p2)
-}
-
 
 pdf(file = paste0(outdir, 'differential_cci_frequency_table.pdf'), width = 13)
 plot_table(all_data)
@@ -155,163 +94,31 @@ dev.off()
 
 
 
-# Plot differential gene cell ranking (sending/receiving)
-# Plotting script from CrossTalkeR (modified)
-plot_rank = function(all_data){
-  curr <- "EXP_x_CTR"
-  curr_net <- all_data@graphs_ggi[[curr]]
-  up_graph <- igraph::subgraph.edges(curr_net, E(curr_net)[E(curr_net)$MeanLR > 0])
-  down_graph <- igraph::subgraph.edges(curr_net, E(curr_net)[E(curr_net)$MeanLR < 0])
-  in_deg_up <- igraph::degree(up_graph, mode = 'in')
-  in_deg_down <- igraph::degree(down_graph, mode = 'in')
-  in_up <- tibble::tibble(genes = paste0(names(in_deg_up),'_up'), values=as.array(in_deg_up))
-  in_down <- tibble::tibble(genes = paste0(names(in_deg_down),'_down'), values=as.array(in_deg_down))
-  in_deg_data_up <-dplyr::top_n(in_up, 10, values)
-  in_deg_data_down <- dplyr::top_n(in_down, 10, values)
-  in_deg_data_down$values <- 0 -in_deg_data_down$values
-  in_deg_data <- dplyr::bind_rows(in_deg_data_up,in_deg_data_down )
-  in_deg_data$Expression <- ifelse(in_deg_data$values <0,'Downregulated','Upregulated')
-  p1 <- ggplot2::ggplot(in_deg_data,ggplot2::aes(x=values,y=reorder(genes,values),fill=Expression))+
-    ggplot2::geom_bar(stat = 'identity',position = "identity")+
-    ggplot2::scale_fill_manual(values=pals::coolwarm(2))+
-    ggplot2::geom_text(ggplot2::aes(label=sub("-", "",values)),size=3.5)+
-    ggplot2::ggtitle('Receiving')+
-    ggplot2::ylab('')+
-    ggplot2::xlab('Number of interactions')+
-    cowplot::theme_cowplot() +
-    theme(axis.line  = element_blank(),
-          axis.ticks = element_blank(),
-          axis.text.x = element_blank())
-  out_deg_up <- igraph::degree(up_graph, mode = 'out')
-  out_deg_down <- igraph::degree(down_graph, mode = 'out')
-  out_up <- tibble::tibble(genes = paste0(names(out_deg_up),'_up'), values=as.array(out_deg_up))
-  out_down <- tibble::tibble(genes = paste0(names(out_deg_down),'_down'), values=as.array(out_deg_down))
-  out_deg_data_up <-dplyr::top_n(out_up, 10, values)
-  out_deg_data_down <- dplyr::top_n(out_down, 10, values)
-  out_deg_data_down$values <- 0-out_deg_data_down$values
-  out_deg_data <- dplyr::bind_rows(out_deg_data_up,out_deg_data_down )
-  out_deg_data$Expression <- ifelse(out_deg_data$values <0,'Downregulated','Upregulated')
-  p2 <- ggplot2::ggplot(out_deg_data,ggplot2::aes(x=values,y=reorder(genes,values),fill=Expression))+
-    ggplot2::geom_bar(stat = 'identity',position = "identity", show.legend = FALSE)+
-    ggplot2::scale_fill_manual(values=pals::coolwarm(2))+
-    ggplot2::geom_text(ggplot2::aes(label=sub("-", "",values)),size=3.5)+
-    ggplot2::ggtitle('Sending')+
-    ggplot2::ylab('')+
-    ggplot2::xlab('Number of interactions')+
-    cowplot::theme_cowplot() +
-    theme(axis.line  = element_blank(),
-          axis.ticks = element_blank(),
-          axis.text.x = element_blank())
-  return(p2+p1)
-}
-
+#---- Differential gene cell ranking (sending/receiving) (active mild vs active severe)
 
 pdf(file = paste0(outdir, 'differential_gene_cell_ranking.pdf'), width = 14)
 plot_rank(all_data)
 dev.off()
 
 
-# Sankey plot of IL10 for recovered mild vs severe
-outdir = 'recovered_mild_vs_recovered_severe/'
-target = c('SELL')
 
-data = data_recov@tables$EXP_x_CTR
-data = data[grep(target, data$allpair),]
-data$Expression = ifelse(data$MeanLR > 0, 'Up', 'Down')
-data$AbsMeanLR = abs(data$MeanLR)
+#----Sankey plots of selected interactions
 
-pdf(file = paste0(outdir, 'IL10_sankey.pdf'), width = 10, height = 5)
-ggplot(data, aes(y = AbsMeanLR,
-                 axis1 = Ligand.Cluster,
-                 axis2 = Ligand,
-                 axis3 = Receptor,
-                 axis4 = Receptor.Cluster)) +
-  geom_alluvium(aes(fill = Expression), width = 1/12) +
-  geom_stratum(width = 1/12) +
-  geom_label(stat = 'stratum', aes(label = after_stat(stratum))) +
-  scale_x_discrete(limits = c('Ligand.Cluster', 'Ligand', 
-                              'Receptor', 'Receptor.Cluster')) +
-  cowplot::theme_cowplot() +
-  theme(axis.ticks = element_blank(),
-        axis.line = element_blank(),
-        axis.title.y = element_blank(), 
-        axis.text.y = element_blank()) +
-  scale_fill_viridis_d()
-dev.off()
+# Active mild vs active severe
+outdir = 'active_mild_vs_active_severe/'
+targets = c('MICB', 'IFNG')
 
-
-
-#---- KEGG pathway analysis
-
-library(clusterProfiler)
-library(org.Hs.eg.db)
-library(patchwork)
-library(dplyr)
-library(viridis)
-data = readRDS(file = paste0(outdir, 'LR_data_final.Rds'))
-cell.types = unique(c(unname(data@tables$EXP_x_CTR$Ligand.Cluster),
-  unname(data@tables$EXP_x_CTR$Receptor.Cluster)))
-
-
-pdf(file = paste0(outdir, 'KEGG_enrichment_significant.pdf'), height = 5, width = 8)
-for (cell.type in cell.types){
-  # Get interactions up/down in active mild vs active severe
-  up = data@tables$EXP_x_CTR %>%
-    filter(MeanLR > 0 & Ligand.Cluster %in%  cell.type)
-  down = data@tables$EXP_x_CTR %>%
-    filter(MeanLR < 0 & Ligand.Cluster %in%  cell.type)
-  
-  # Get unique up/down interactions
-  up_exclu =  unique(up$Ligand)[match(unique(up$Ligand),unique(down$Ligand), nomatch = -1) == -1]
-  down_exclu = unique(down$Ligand)[match(unique(down$Ligand),unique(up$Ligand), nomatch = -1) == -1]
-  
-  # KEGG enrichment (up)
-  if (length(up_exclu) != 0){
-    genes_up =  bitr(up_exclu, fromType = 'SYMBOL', toType = c('ENTREZID', 'ENSEMBL'), OrgDb = org.Hs.eg.db)
-    enrich_up = enrichKEGG(genes_up$ENTREZID, organism = 'hsa')
-    
-    # Plot significant results
-    plot_data = enrich_up@result %>%
-      mutate(log10.p.adjust = -log10(p.adjust)) %>%
-      filter(p.adjust < 0.05) %>%
-      top_n(n = 50, wt = log10.p.adjust)
-    
-    p_up = ggplot(plot_data, aes(x = log10.p.adjust, 
-                                 y = reorder(Description,log10.p.adjust))) +
-      geom_bar(stat = 'identity', fill = viridis(2)[2]) +
-      cowplot::theme_cowplot() +
-      theme(axis.ticks = element_blank(),
-            axis.title.y = element_blank(),
-            axis.text.x = element_text(size = 10),
-            axis.text.y = element_text(size = 10),
-            title = element_text(size = 12)) +
-      labs(x = bquote(~-Log[10]~'(pAdj)'),
-           title = paste0(cell.type, ': Up'))
-    print(p_up)
-  }
-  
-  # KEGG enrichment (down)
-  if(length(down_exclu) != 0){
-    genes_dw =  bitr(down_exclu, fromType = 'SYMBOL', toType = c('ENTREZID', 'ENSEMBL'), OrgDb = org.Hs.eg.db)
-    enrich_dw = enrichKEGG(genes_dw$ENTREZID, organism = 'hsa')
-    plot_data = enrich_dw@result %>%
-      mutate(log10.p.adjust = -log10(p.adjust)) %>%
-      filter(p.adjust < 0.05) %>%
-      top_n(n = 50, wt = log10.p.adjust)
-    
-    p_dw = ggplot(plot_data, aes(x = log10.p.adjust, 
-                                 y = reorder(Description,log10.p.adjust))) +
-      geom_bar(stat = 'identity', fill = viridis(2)[1]) +
-      cowplot::theme_cowplot() +
-      theme(axis.ticks = element_blank(),
-            axis.title.y = element_blank(),
-            axis.text.x = element_text(size = 10),
-            axis.text.y = element_text(size = 10),
-            title = element_text(size = 12)) +
-      labs(x = bquote(~-Log[10]~'(pAdj)'),
-           title = paste0(cell.type, ': Down'))
-    print(p_dw)
-  }
+for (target in targets){
+  pdf(file = paste0(outdir, 'sankey_', target, '.pdf'), width = 10, height = 5)
+  print(plot_sankey(data = data_active@tables$EXP_x_CTR, target = target))
+  dev.off()
 }
+
+# Recovered mild vs recovered severe
+outdir = 'recovered_mild_vs_recovered_severe/'
+target = 'SELL'
+
+pdf(file = paste0(outdir, 'sankey_', target, '.pdf'), width = 10, height = 5)
+plot_sankey(data = data_recov@tables$EXP_x_CTR, target = target)
 dev.off()
 
---
+++ sc_source/crosstalker_source.R
@@ -0,0 +1,153 @@
+# Copyright (c) [2020] [Monica T. Hannani]
+# mhannani@ukaachen.de
+
+
+#---- Generate CrossTalkeR report
+
+crosstalker_report = function(Control, Case, Genes, Indir, Outdir){
+  # Define path for control and case (CellPhoneDB has been run separately on these)
+  # Files have already been filtered and corrected with format.py from CrossTalkeR package
+  paths = c('CTR' = paste0(Indir, Control, '_filtered_corrected.csv'),
+            'EXP' = paste0(Indir, Case, '_filtered_corrected.csv'))
+  
+  # Generate report
+  outdir = paste0(Outdir, '/', Control, '_vs_', Case, '/')
+  
+  generate_report(lrpaths = paths,
+                  out_path = outdir,
+                  threshold = 0,
+                  out_file = paste0(Control, '_vs_', Case, '.html'),
+                  report = TRUE)
+}
+
+
+
+
+#---- Differential frequency table (modified from CrossTalkeR)
+plot_table = function(all_data = all_data){
+  curr <- "EXP_x_CTR"
+  curr_net <- all_data@graphs[[curr]]
+  in_deg_up <- table(all_data@tables[[curr]]$Ligand.Cluster[all_data@tables[[curr]]$MeanLR > 0])
+  in_up <- tibble::tibble(as.data.frame(in_deg_up))
+  in_deg_down <- table(all_data@tables[[curr]]$Ligand.Cluster[all_data@tables[[curr]]$MeanLR < 0])
+  in_down <- tibble::tibble(as.data.frame(in_deg_down))
+  in_down$Freq <- 0-in_down$Freq
+  in_all <- dplyr::bind_rows(in_up,in_down)
+  in_all$Expression <- ifelse(in_all$Freq<0,'Downregulated','Upregulated')
+  in_all$rank <- ifelse(in_all$Freq<0,0,1)
+  out_deg_up <- table(all_data@tables[[curr]]$Receptor.Cluster[all_data@tables[[curr]]$MeanLR > 0])
+  out_up <- tibble::tibble(as.data.frame(out_deg_up))
+  out_deg_down <- table(all_data@tables[[curr]]$Receptor.Cluster[all_data@tables[[curr]]$MeanLR < 0])
+  out_down <- tibble::tibble(as.data.frame(out_deg_down))
+  out_down$Freq <- 0-out_down$Freq
+  out_all <- dplyr::bind_rows(out_up,out_down)
+  out_all$Expression <- ifelse(out_all$Freq<0,'Downregulated','Upregulated')
+  out_all$rank <- ifelse(out_all$Freq<0,0,1)
+  p1 <- ggplot2::ggplot(in_all,ggplot2::aes(x=Freq,y=Var1,fill=Expression))+
+    ggplot2::geom_bar(stat = 'identity',position = "identity", show.legend = FALSE)+
+    ggplot2::geom_text(ggplot2::aes(label=sub("-", "", Freq)),size=3.5)+
+    ggplot2::scale_fill_manual(values=pals::coolwarm(2))+
+    ggplot2::ggtitle('Ligands')+
+    ggplot2::ylab('')+
+    ggplot2::xlab('Number of interactions')+
+    cowplot::theme_cowplot() +
+    theme(axis.line  = element_blank(),
+          axis.ticks = element_blank(),
+          axis.text.x = element_blank())
+  p2 <- ggplot2::ggplot(out_all,ggplot2::aes(x=Freq,y=Var1,fill=Expression))+
+    ggplot2::geom_bar(stat = 'identity',position = "identity")+
+    ggplot2::geom_text(ggplot2::aes(label=sub("-", "", Freq)),size=3.5)+
+    ggplot2::scale_fill_manual(values=pals::coolwarm(2))+
+    ggplot2::ggtitle('Receptors')+
+    ggplot2::ylab('')+
+    ggplot2::xlab('Number of interactions')+
+    cowplot::theme_cowplot() +
+    theme(axis.line  = element_blank(),
+          axis.ticks = element_blank(),
+          axis.text.x = element_blank())
+  return(p1+p2)
+}
+
+
+
+
+#---- Differential gene cell ranking (modified from CrossTalkeR)
+
+plot_rank = function(all_data){
+  curr <- "EXP_x_CTR"
+  curr_net <- all_data@graphs_ggi[[curr]]
+  up_graph <- igraph::subgraph.edges(curr_net, E(curr_net)[E(curr_net)$MeanLR > 0])
+  down_graph <- igraph::subgraph.edges(curr_net, E(curr_net)[E(curr_net)$MeanLR < 0])
+  in_deg_up <- igraph::degree(up_graph, mode = 'in')
+  in_deg_down <- igraph::degree(down_graph, mode = 'in')
+  in_up <- tibble::tibble(genes = paste0(names(in_deg_up),'_up'), values=as.array(in_deg_up))
+  in_down <- tibble::tibble(genes = paste0(names(in_deg_down),'_down'), values=as.array(in_deg_down))
+  in_deg_data_up <-dplyr::top_n(in_up, 10, values)
+  in_deg_data_down <- dplyr::top_n(in_down, 10, values)
+  in_deg_data_down$values <- 0 -in_deg_data_down$values
+  in_deg_data <- dplyr::bind_rows(in_deg_data_up,in_deg_data_down )
+  in_deg_data$Expression <- ifelse(in_deg_data$values <0,'Downregulated','Upregulated')
+  p1 <- ggplot2::ggplot(in_deg_data,ggplot2::aes(x=values,y=reorder(genes,values),fill=Expression))+
+    ggplot2::geom_bar(stat = 'identity',position = "identity")+
+    ggplot2::scale_fill_manual(values=pals::coolwarm(2))+
+    ggplot2::geom_text(ggplot2::aes(label=sub("-", "",values)),size=3.5)+
+    ggplot2::ggtitle('Receiving')+
+    ggplot2::ylab('')+
+    ggplot2::xlab('Number of interactions')+
+    cowplot::theme_cowplot() +
+    theme(axis.line  = element_blank(),
+          axis.ticks = element_blank(),
+          axis.text.x = element_blank())
+  out_deg_up <- igraph::degree(up_graph, mode = 'out')
+  out_deg_down <- igraph::degree(down_graph, mode = 'out')
+  out_up <- tibble::tibble(genes = paste0(names(out_deg_up),'_up'), values=as.array(out_deg_up))
+  out_down <- tibble::tibble(genes = paste0(names(out_deg_down),'_down'), values=as.array(out_deg_down))
+  out_deg_data_up <-dplyr::top_n(out_up, 10, values)
+  out_deg_data_down <- dplyr::top_n(out_down, 10, values)
+  out_deg_data_down$values <- 0-out_deg_data_down$values
+  out_deg_data <- dplyr::bind_rows(out_deg_data_up,out_deg_data_down )
+  out_deg_data$Expression <- ifelse(out_deg_data$values <0,'Downregulated','Upregulated')
+  p2 <- ggplot2::ggplot(out_deg_data,ggplot2::aes(x=values,y=reorder(genes,values),fill=Expression))+
+    ggplot2::geom_bar(stat = 'identity',position = "identity", show.legend = FALSE)+
+    ggplot2::scale_fill_manual(values=pals::coolwarm(2))+
+    ggplot2::geom_text(ggplot2::aes(label=sub("-", "",values)),size=3.5)+
+    ggplot2::ggtitle('Sending')+
+    ggplot2::ylab('')+
+    ggplot2::xlab('Number of interactions')+
+    cowplot::theme_cowplot() +
+    theme(axis.line  = element_blank(),
+          axis.ticks = element_blank(),
+          axis.text.x = element_blank())
+  return(p2+p1)
+}
+
+
+
+
+#---- Sankey plots of interactions from CrossTalkeR
+
+plot_sankey = function(data, target){
+  data = data[grep(target, data$allpair),]
+  data$Expression = ifelse(data$MeanLR > 0, 'Up', 'Down')
+  data$AbsMeanLR = abs(data$MeanLR)
+  
+  p = ggplot(data, aes(y = AbsMeanLR,
+                       axis1 = Ligand.Cluster,
+                       axis2 = Ligand,
+                       axis3 = Receptor,
+                       axis4 = Receptor.Cluster)) +
+    geom_alluvium(aes(fill = Expression), width = 1/12) +
+    geom_stratum(width = 1/12) +
+    geom_label(stat = 'stratum', aes(label = after_stat(stratum))) +
+    scale_x_discrete(limits = c('Ligand.Cluster', 'Ligand', 
+                                'Receptor', 'Receptor.Cluster')) +
+    cowplot::theme_cowplot() +
+    theme(axis.ticks = element_blank(),
+          axis.line = element_blank(),
+          axis.title.y = element_blank(), 
+          axis.text.y = element_blank()) +
+    scale_fill_viridis_d()
+  
+  return(p)
+}
+
